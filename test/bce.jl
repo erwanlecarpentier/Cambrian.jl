de8ecd53bb65777f4fb64dd7fbfabb4f217a6ffc
erwanlecarpentier <erwan.lc2@gmail.com>
2021-10-29 12:16:15 +0200

    Clustering method in behavior space using k-means algorithm

--- test/bce.jl
+++ test/bce.jl
@@ -22,19 +22,43 @@ function Ind(cfg::NamedTuple)
     Ind(rand(cfg.n_genes), -Inf*ones(cfg.d_fitness), zeros(cfg.d_behavior))
 end
 
+function copyind(ind::Ind)
+    Ind(copy(ind.genes), copy(ind.fitness), copy(ind.behavior))
+end
+
+function subargmin(x::Vector{Float64}, b::BitArray)
+    min_value = +Inf
+    min_index = 0
+    for (xi, bi, i) in zip(x, b, eachindex(x))
+        (bi && xi < min_value) || continue
+        min_value = xi
+        min_index = i
+    end
+    return min_index
+end
+
+"""
+    cluster(e::BCEEvo)
+
+A clustering method finding best representatives of the k-means clustering
+algorithm applied on the behavior descriptors.
+"""
 function cluster(e::BCEEvo)
     X = reduce(hcat, [e.population[i].behavior for i in eachindex(e.population)])
-    R = kmeans(X, e.cfg.n_emitters; maxiter=200, display=:iter)
-    # TODO complete from here
-    # Find best representative for each category
-    [population[1]] # TODO return best representatives
+    R = kmeans(X, e.cfg.n_emitters; maxiter=200) #, display=:iter)
+    repre = Vector{Ind}()
+    for i in 1:e.cfg.n_emitters
+        repre_index = subargmin(R.costs, R.assignments .== i)
+        push!(repre, copyind(e.population[repre_index]))
+    end
+    repre
 end
 
 cfgpath = joinpath(dirname(@__DIR__), "cfg", "bce.yaml")
 cfg = get_config(cfgpath)
 
 fitness(ind::Ind) = [(cos(2.0*π*maximum(ind.genes))+1.0)/2.0]
-behavior(ind::Ind) = [mean(ind.genes)]
+behavior(ind::Ind) = [mean(ind.genes), maximum(ind.genes)]
 
 function test_bce_evo(e::BCEEvo)
     @test all([e.population[i].fitness[1] for i in eachindex(e.population)] .> -Inf)
@@ -46,8 +70,6 @@ end
     @test length(e.population[1].genes) == cfg.n_genes
     test_bce_evo(e)
 end
-
-
 
 @testset "BCE Evolution with custom initialization function" begin
     initgenes = [0.0, 0.0]
